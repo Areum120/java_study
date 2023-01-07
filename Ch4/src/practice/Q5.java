988c8aed97503bfeaa55005d4ef431dc86741dd7
LAPTOP-F3LFLS66\tjfsu <tjfsu120@gmail.com>
2023-01-07 17:43:26 +0900

    print diamondsStars update

--- Ch4/src/practice/Q5.java
+++ Ch4/src/practice/Q5.java
@@ -1,6 +1,48 @@
 package practice;
 
+
 public class Q5 {
+
+    String space = " ";
+    String star = "*";
+    int num;
+
+    public Q5(){
+
+    }
+
+    public Q5(int n){
+        this.num = n;
+    }
+
+    public void printStar(int n){
+        int i,j;
+        if(n>=2 || n%2==0) {//n이 최소 2보다 크고 짝수일 경우
+            for (i = 0; i <= n; i++) {
+                if (i <= n/2) {
+                    for (j = 0; j <= (n/2) - i; j++) {
+                        System.out.print(" ");
+                    }
+                    for (j = 1; j <= 2 * i + 1; j++) {
+                        System.out.print("*");
+                    }
+                } else {
+                    for (j = 0; j <= (i - (n/2)); j++) {
+                        System.out.print(" ");
+                    }
+                    for (j = 1; j <= ((-2*i) + (2*n+1)); j++) {
+                        System.out.print("*");
+                    }
+                }
+                System.out.println();
+            }
+        }
+        else{
+            System.out.print("다이아몬드가 출력되지 않습니다.");
+        }
+
+    }
+
     public static void main(String[] args) {
         //반복문 사용하여 모양 출력
 //         *
@@ -10,24 +52,8 @@ public class Q5 {
 //       *****
 //        ***
 //         *
-        int i,j;
-        for(i=0; i<=6; i++) {
-            if (i <= 3) {
-                for (j = 0; j <= 3 - i; j++) {
-                    System.out.print(" ");
-                }
-                for (j = 1; j <= 2 * i + 1; j++) {
-                    System.out.print("*");
-                }
-            } else {
-                for (j = 3; j <= i; j++) {
-                    System.out.print(" ");
-                }
-                for (j = 3; j <= 2 * i - 1; j++) {
-                    System.out.print("*");
-                }
-            }
-            System.out.println();
-        }
+        Q5 q5 = new Q5();
+        q5.printStar(40);
+
     }
 }
